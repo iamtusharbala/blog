c34ae55d2b682063a19d9d6149d8b6ce63980fb0
iamtusharbala <tusharbala284@gmail.com>
2025-10-09 03:17:55 +0530

    h tags configured

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -7,16 +7,17 @@ function About() {
     <section className="about d-flex justify-content-evenly">
       <div className="row">
         <div className="col-lg-8 d-flex flex-column align-items-start justify-content-end">
-        <p class="text-start section-heading">About</p>
-        <p class="text-start section-text">
+        <h2 className="text-start section-heading">About</h2>
+        <h1 className="text-start section-text">
           I'm Tushar Balakrishnan, a <span className="bold">ServiceNow Developer</span> dedicated to creating
-          efficient, scalable, and user-focused solutions. I specialize in
+          efficient, scalable, and user-focused solutions. 
+        </h1>
+        <h2 className="text-start section-text">I specialize in
           leveraging the ServiceNow platform to streamline workflows, automate
           processes, and solve real-world business challenges. With a strong
           passion for continuous learning and clean coding practices, I strive
           to bridge the gap between technology and business needs while growing
-          into a well-rounded and impactful software professional.
-        </p>
+          into a well-rounded and impactful software professional.</h2>
       </div>
       <div className="col-lg-4">
         <img src={img} class="img-fluid" alt="Tushar Image" />
