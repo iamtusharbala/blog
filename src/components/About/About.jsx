c59d44cd6d9c14839c2fec5a751f4a4b7df803b1
iamtusharbala <tusharbala284@gmail.com>
2025-10-09 12:11:39 +0530

    fixed ddd section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,14 +5,14 @@ import img from "../../assets/images/IMG_3762.webp";
 function About() {
   return (
     <section className="about d-flex justify-content-evenly">
-      <div className="row">
+      <div className="row w-100">
         <div className="col-lg-8 d-flex flex-column align-items-start justify-content-end">
-        <h2 className="text-start section-heading">About</h2>
-        <h1 className="text-start section-text">
+        <h2 className="section-heading">About</h2>
+        <h1 className="section-text text-justify">
           I'm <span className="bold">Tushar Balakrishnan</span>, a <span className="bold">ServiceNow Developer</span> dedicated to creating
           efficient, scalable, and user-focused solutions. 
         </h1>
-        <h2 className="text-start section-text">I specialize in
+        <h2 className="section-text text-justify">I specialize in
           leveraging the ServiceNow platform to streamline workflows, automate
           processes, and solve real-world business challenges. With a strong
           passion for continuous learning and clean coding practices, I strive
--- src/components/DDD section/Developer.jsx
+++ src/components/DDD section/Developer.jsx
@@ -1,13 +1,26 @@
-import React from 'react'
-import './Developer.css'
+import React from "react";
+import "./Developer.css";
 
 function Developer() {
   return (
-    <section className='my-5'>
-        <p className='text-center section-heading-ddd '>Developer<span className='dot-red'>.</span> Dreamer<span className='dot-red'>.</span> Coder <span className='ddc-code'>&lt;/&gt;</span> </p>
-        <p className='text-center section-text mt-3'>I build on ServiceNow — clean code, smart workflows.</p>
+    <section className="my-5">
+      {/* The main heading container */}
+      <p
+        className="text-center section-heading-ddd mb-0"
+        style={{ wordBreak: "break-word", overflowWrap: "break-word" }}
+      >
+        <span className="fs-1 fs-sm-3">
+          Developer<span className="dot-red">.</span> Dreamer
+          <span className="dot-red">.</span> Coder{" "}
+          <span className="ddc-code">&lt;/&gt;</span>
+        </span>
+      </p>
+
+      <p className="text-center section-text mt-3 fs-5 fs-sm-6">
+        I build on ServiceNow — clean code, smart workflows.
+      </p>
     </section>
-  )
+  );
 }
 
-export default Developer
+export default Developer;
